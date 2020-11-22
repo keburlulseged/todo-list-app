75637684e0a697830b25cb78a237e955bbae62c0
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-21 21:50:14 -0800

    Add rendering and input foundation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Todo from './components/Todo'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "Take out the trash",
+      description: "The ben men come at 9pm"
+    },
+    {
+      title: "Walk the dog",
+      description: "He is bored"
+    },
+    {
+      title: "Clean the crib",
+      description: "It's getting messy"
+    },
+    {
+      title: "Dominate the day",
+      description: "Only you can change your life"
+    }
+  ]);
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-do App</h1>
+      <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+      <button>Add Todo</button>
+      {
+        todos.map(todo => (
+          <Todo title={todo.title} description={todo.description} />
+        ))
+      }
+
     </div>
   );
 }
