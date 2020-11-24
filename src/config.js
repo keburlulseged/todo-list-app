59c55940b5004cd2e19f440d3fd7eba1d8e8d0ec
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-23 23:19:45 -0800

    Clean up and add deploy script

---
+++ src/config.js
@@ -0,0 +1,4 @@
+var config = {
+  apiKeyFirebase: "AIzaSyAXGUg6kho7EX6Srcgoxf0OUSj-bFyZWZg",
+  appIdFirebase: "1:853742614330:web:965520f400def2eaf0c203"
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAXGUg6kho7EX6Srcgoxf0OUSj-bFyZWZg",
+  apiKey: process.env.FIREBASE_API,
   authDomain: "todo-list-app-b4b6f.firebaseapp.com",
   databaseURL: "https://todo-list-app-b4b6f.firebaseio.com",
   projectId: "todo-list-app-b4b6f",
   storageBucket: "todo-list-app-b4b6f.appspot.com",
   messagingSenderId: "853742614330",
-  appId: "1:853742614330:web:965520f400def2eaf0c203",
+  appId: process.env.FIREBASE_ID ,
   measurementId: "G-4SCMKTTW7W"
 });
 
